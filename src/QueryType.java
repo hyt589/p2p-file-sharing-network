946105a4469db9dc5dd98e1b3ea9ec9935a5f3e4
Yutong Huang <yxh589@case.edu>
2019-11-02 02:57:17 -0400

    ...

--- src/QueryType.java
+++ src/QueryType.java
@@ -1,7 +1,6 @@
 /**
  * Represents different types of queries.
  * <p>Q,R,T are as specified in project requirements</p>
- * <p>E represents an echo query sent to check connection status</p>
  */
 public enum QueryType {
     Q,R,T;
