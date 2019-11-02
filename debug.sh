4a53e2094696541c243f7ff9fc74ff8d2ddf8808
Yutong Huang <yxh589@case.edu>
2019-11-02 00:01:50 -0400

    minor bug fix

--- debug.sh
+++ debug.sh
@@ -1,1 +1,1 @@
-java -cp classes -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=0.0.0.0:5005 p2p
+java -cp classes -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=127.0.0.1:5005 p2p
