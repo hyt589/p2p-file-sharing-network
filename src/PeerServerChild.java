7678e70989aa0b0ebf07c14f22938dd45829c77e
Yutong Huang <yxh589@case.edu>
2019-11-01 21:23:47 -0400

    minor bug fix

--- src/PeerServerChild.java
+++ src/PeerServerChild.java
@@ -65,7 +65,7 @@ public class PeerServerChild extends Thread {
                     + config.getPeerConfig().get("file_sender_port"), filename));
             out.writeBytes(response.toString() + "\n");
         }else { //forward the query to other neighbors
-            if (query.id < id) {
+            if (query.id == id) {
                 System.out.printf("Query id:%d is smaller than current query count %d%n", query.id, Query.getCount().get());
                 System.out.println("Abort forwarding to avoid broadcast storm");
                 return;//do not forward if query id is smaller than Query.count
